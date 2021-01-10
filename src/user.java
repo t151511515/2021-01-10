8c65b712d07615536e12fd92d91dea1aa2b47784
Administrator <1456344203@qq.com>
2021-01-10 20:17:50 +0800

    测试

--- src/user.java
+++ src/user.java
@@ -1,22 +1,4 @@
 public class user {
     int id;
-    int password;
-    int sex;
-    int high;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-    public void setPassword(int password) {
-        this.password = password;
-    }
 }
